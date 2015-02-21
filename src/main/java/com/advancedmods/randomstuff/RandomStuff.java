03b5d5b539bd0f670aa74f8ae0f1b3f61f1d22e3
Dennisbonke <dennisb@kabelfoon.net>
2015-02-21 11:58:04 +0100

    Added gradle files and main class.

---
+++ src/main/java/com/advancedmods/randomstuff/RandomStuff.java
@@ -0,0 +1,7 @@
+package com.advancedmods.randomstuff;
+
+/**
+ * Created by Dennisbonke on 21-2-2015.
+ */
+public class RandomStuff {
+}
---
+++ src/main/java/com/advancedmods/randomstuff/core/RSProps.java
@@ -0,0 +1,7 @@
+package com.advancedmods.randomstuff.core;
+
+/**
+ * Created by Dennisbonke on 21-2-2015.
+ */
+public class RSProps {
+}
